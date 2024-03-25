6f683ccd5fdf21f2aef69cced2f44cec5ad987d5
natalia25121993 <164492889+natalia25121993@users.noreply.github.com>
2024-03-25 15:04:46 +0300

    Add files via upload

---
+++ 1 homo sap.py
@@ -0,0 +1,10 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\программирование\python\homo sap.py
+1 этап австралопитек
+2 этап человек умелый
+3 этап человек прямоходящий
+4 этап палеоантолог
+5 этап homo sapiens
+ австралопитек=> человек умелый=> человек прямоходящий=> палеоантолог=> homo sapiens
