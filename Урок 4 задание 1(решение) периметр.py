d645a87b23551ed1268b3125fe8bcfb472f67edd
natalia25121993 <164492889+natalia25121993@users.noreply.github.com>
2024-03-26 13:30:35 +0300

    Add files via upload

---
+++ Урок 4 задание 1(решение) периметр.py
@@ -0,0 +1,7 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\программирование\python\домашняя\1 homo sap.py
+6
+4
+24.0 20.0
